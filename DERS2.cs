1df685acebef266366af29e0101f18281733d5ff
betlsari <147310447+betlsari@users.noreply.github.com>
2024-11-27 18:00:14 +0300

    Create DERS2.cs

---
+++ DERS2.cs
@@ -0,0 +1,31 @@
+#region
+            //double exam1, exam2, exam3, result;
+            //Console.WriteLine("1.Sınav Notunu Giriniz: ");
+            //exam1 = double.Parse(Console.ReadLine());
+
+            //Console.WriteLine("2.Sınav Notunu Giriniz: ");
+            //exam2 = double.Parse(Console.ReadLine());
+
+            //Console.WriteLine("3.Sınav Notunu Giriniz: ");
+            //exam3 = double.Parse(Console.ReadLine());
+
+            //result = (exam1 + exam2 + exam3) / 3;
+            //Console.WriteLine("Ortlama: "+result);
+            #endregion
+
+            #region
+
+            //char gender;
+            //Console.WriteLine("Lütfen cinsiyet giriniz: ");
+            //gender= char.Parse(Console.ReadLine());
+            //Console.WriteLine("Seçtiğiniz cinsiyet: "+gender);
+
+
+
+            #endregion
+
+
+            Console.Read();
+        }
+    }
+}
