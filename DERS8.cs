d81af55e69617912583975ab2c71464e212f1076
betlsari <147310447+betlsari@users.noreply.github.com>
2024-11-27 18:09:08 +0300

    Create DERS8.cs

---
+++ DERS8.cs
@@ -0,0 +1,106 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metotlar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region  Void Metotlar
+            //void customerList()
+            //{
+            //    Console.WriteLine("Ali Yıldız");
+            //    Console.WriteLine("Mehmet Yeni");
+            //    Console.WriteLine("Gurur Yıldız");
+            //    Console.WriteLine("Melek Kıldız");
+            //}
+            //customerList();
+            //customerList();
+            //customerList();
+            //customerList();
+            
+            #endregion
+            #region Geriye Değer Döndürmeyen String Parametreli Metotlar
+            //void writeMethod(string customerName)
+            //{
+            //    Console.WriteLine(customerName);
+            //}
+            //writeMethod("mehmet yıldız");
+
+            //void customerCard(string name, string surName)
+            //{
+            //    Console.WriteLine("Müşteri: " + name + " " + surName);
+            //}
+
+            //customerCard("Ali", "Hayrı");
+
+
+
+
+            #endregion
+            #region Geriye Değer Döndürmeyen Int Parametreli Metotlar
+            //void sum(int num1, int num2)
+            //{
+            //    int result = num1 + num2;
+            //    Console.WriteLine(result);
+            //}
+            //sum(5, 6);
+            #region Geriye Değer Döndüren Metotlar
+            ////string customerName()
+            ////{
+            ////    return "Buse Yıldız";
+            ////}
+            ////Console.WriteLine(customerName());
+            #endregion
+            #region Geriye Değer Döndüren String Parametreli Metotlar
+            ////string countryCard(string countryName, string flagColor)
+            ////{
+            ////    string cardInfo = ("Ülke: " + countryName + "Bayrak Rengi: " + flagColor);
+
+            ////    return cardInfo;
+            ////}
+            ////string x, y;
+            ////Console.WriteLine("Ülke Adını giriniz: ");
+            ////x= Console.ReadLine();
+
+            ////Console.WriteLine( "Bayrak rengini giriniz: ");
+            ////y= Console.ReadLine();
+
+            ////Console.WriteLine(countryCard(x, y));
+            ////Console.WriteLine(CountryCard("Türkiye","Aankara","Kırmızı Beyaz"));
+            #endregion
+            #region Örnek Metot
+
+            //void sum(int num1, int num2)
+            //{
+            //    int result = num1 + num2;
+            //    Console.WriteLine(result);
+            //}
+            //sum(5, 6);
+            //#endregion
+           #region Örnek
+
+            //int examResult(string name,int exam1,int exam2)
+            //{
+            //    int average = (exam1 + exam2) / 2;
+            //    if (average <= 50)
+            //    {
+            //        Console.WriteLine("Öğrenci kaldı");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Öğrenci geçti");
+            //    }
+            //    return average;
+            //}
+            //examResult("Ali",50,30);
+            #endregion
+            //Console.Read();
+        }
+    }
+}
+}
